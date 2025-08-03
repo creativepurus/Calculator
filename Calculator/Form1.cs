8bb3bb5472272bdd97716d202abd122460cbbcdf
Jalal Jaleh <jalaljaleh@gmail.com>
2025-08-03 12:50:16 +0330

    A clean and optimized code

--- Calculator/Form1.cs
+++ Calculator/Form1.cs
@@ -1,140 +1,65 @@
 ﻿using System;
+using System.Linq;
 using System.Windows.Forms;
 
 namespace Calculator
 {
     public partial class Calculator : Form
     {
+        private float number1;
+        private float number2;
+        private float result;
+        private string operation;
+
         public Calculator()
         {
             InitializeComponent();
+
+            foreach (var btn in new[] { BtnNumber0, BtnNumber1, BtnNumber2, BtnNumber3, BtnNumber4, BtnNumber5, BtnNumber6, BtnNumber7, BtnNumber8, BtnNumber9 })
+                btn.Click += Numbers_Click;
+
+            foreach (var btn in new[] { BtnPlus, BtnMinus, BtnMultiply, BtnDivide })
+                btn.Click += Operators_Click;
+
+            BtnResult.Click += BtnResult_Click;
+
+            BtnClear.Click += (s, e) => ClearFields();
         }
 
-        float num1;
-        float num2;
-        float result;
-        string operation;
-        string divByZeroError;
-
-        private void button1_Click(object sender, EventArgs e)
+        private void Numbers_Click(object sender, EventArgs e)
         {
-            textBox1.Text = textBox1.Text + button1.Text;
+            TbDisplay.Text += ((Button)sender).Text;
         }
 
-        private void button2_Click(object sender, EventArgs e)
+        private void Operators_Click(object sender, EventArgs e)
         {
-            textBox1.Text = textBox1.Text + "2";
+            float.TryParse(TbDisplay.Text, out number1);
+            operation = ((Button)sender).Text;
+            TbDisplay.Clear();
         }
 
-        private void button3_Click(object sender, EventArgs e)
+        private void BtnResult_Click(object sender, EventArgs e)
         {
-            textBox1.Text = textBox1.Text + button3.Text;
-        }
+            if (!float.TryParse(TbDisplay.Text, out number2)) return;
 
-        private void button4_Click(object sender, EventArgs e)
-        {
-            textBox1.Text = textBox1.Text + button4.Text;
-        }
-
-        private void button5_Click(object sender, EventArgs e)
-        {
-            textBox1.Text = textBox1.Text + button5.Text;
-        }
-
-        private void button6_Click(object sender, EventArgs e)
-        {
-            textBox1.Text = textBox1.Text + button6.Text;
-        }
-
-        private void button7_Click(object sender, EventArgs e)
-        {
-            textBox1.Text = textBox1.Text + button7.Text;
-        }
-
-        private void button8_Click(object sender, EventArgs e)
-        {
-            textBox1.Text = textBox1.Text + button8.Text;
-        }
-
-        private void button9_Click(object sender, EventArgs e)
-        {
-            textBox1.Text = textBox1.Text + button9.Text;
-        }
-
-        private void button10_Click(object sender, EventArgs e)     // Button 0
-        {
-            textBox1.Text = textBox1.Text + button10.Text;
-        }
-
-        private void button13_Click(object sender, EventArgs e)     // Plus Operator
-        {
-            operation = "+";
-            num1 = int.Parse(textBox1.Text);
-            textBox1.Clear();
-        }
-
-        private void button14_Click(object sender, EventArgs e)     // Minus Operator
-        {
-            operation = "-";
-            num1 = int.Parse(textBox1.Text);
-            textBox1.Clear();
-        }
-
-        private void button15_Click(object sender, EventArgs e)     // Multiplication Operator
-        {
-            operation = "*";
-            num1 = int.Parse(textBox1.Text);
-            textBox1.Clear();
-        }
-
-        private void button16_Click(object sender, EventArgs e)     // Division Operator
-        {
-            operation = "/";
-            num1 = int.Parse(textBox1.Text);
-            textBox1.Clear();
-        }
-
-        private void button11_Click(object sender, EventArgs e)     // Equals Operator
-        {
-            num2 = int.Parse(textBox1.Text);
-
-            if (operation == "+")
-                result = num1 + num2;
-
-            if (operation == "-")
-                result = num1 - num2;
-
-            if (operation == "*")
-                result = num1 * num2;
-
-            if (operation == "/")
+            result = operation switch
             {
-                if (num2 != 0)
-                {
-                    result = num1 / num2;
-                }
+                "+" => number1 + number2,
+                "-" => number1 - number2,
+                "*" => number1 * number2,
+                "/" when number2 != 0 => number1 / number2,
+                "/" => float.NaN,
+                _ => 0
+            };
 
-                else
-                {
-                    divByZeroError = "Division by Zero Error";
-                    textBox1.Text = divByZeroError + "";
-                }
-            }
-
-            textBox1.Text = result + "";
+            TbDisplay.Text = float.IsNaN(result) ? "Divide by zero" : result.ToString();
         }
 
-        private void button12_Click(object sender, EventArgs e)     // Clear Button
+        private void ClearFields()
         {
-            textBox1.Clear();
-            result = 0;
-            num1 = 0;
-            num2 = 0;
-        }
-
-        private void textBox1_TextChanged(object sender, EventArgs e)       // Display Box
-        {
-
+            TbDisplay.Clear();
+            number1 = number2 = result = 0;
+            operation = string.Empty;
         }
     }
 }
